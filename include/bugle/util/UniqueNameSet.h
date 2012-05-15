dd013b13ef5d02dcf7e580722b07636cdcc31f87
Peter Collingbourne <pcc03@doc.ic.ac.uk>
2012-05-15 16:59:09 +0100

    Add UniqueNameSet

---
+++ include/bugle/util/UniqueNameSet.h
@@ -0,0 +1,18 @@
+#ifndef BUGLE_UTIL_UNIQUENAMESET_H
+#define BUGLE_UTIL_UNIQUENAMESET_H
+
+#include "llvm/ADT/StringRef.h"
+#include "llvm/ADT/StringSet.h"
+
+namespace bugle {
+
+class UniqueNameSet {
+  llvm::StringSet<> Names;
+
+public:
+  std::string makeName(llvm::StringRef OrigName);
+};
+
+}
+
+#endif
---
+++ lib/Util/UniqueNameSet.cpp
@@ -0,0 +1,19 @@
+#include "bugle/util/UniqueNameSet.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace bugle;
+using namespace llvm;
+
+std::string UniqueNameSet::makeName(StringRef OrigName) {
+  if (Names.insert(OrigName))
+    return OrigName;
+
+  unsigned i = 0;
+  while (true) {
+    std::string S = OrigName;
+    llvm::raw_string_ostream SS(S);
+    SS << i;
+    if (Names.insert(SS.str()))
+      return S;
+  }
+}
