9cde09bd8497b34f79052dfb9f7fdac3132a8ead
Peter Collingbourne <peter@pcc.me.uk>
2012-07-03 00:14:56 +0100

    Add globaldce optzn to eliminate unused functions after inlining

--- blang.sh
+++ blang.sh
@@ -3,5 +3,5 @@
 BASE=`basename $1 .c`
 
 @LLVM_BINDIR@/clang -I@CMAKE_SOURCE_DIR@/include -emit-llvm -c -o $BASE.bc "$@"
-@LLVM_BINDIR@/opt -mem2reg -o $BASE.opt.bc $BASE.bc
+@LLVM_BINDIR@/opt -mem2reg -globaldce -o $BASE.opt.bc $BASE.bc
 @CMAKE_BINARY_DIR@/bugle -o $BASE.bpl $BASE.opt.bc
--- cublang.sh
+++ cublang.sh
@@ -3,5 +3,5 @@
 BASE=`basename $1 .cu`
 
 @LLVM_BINDIR@/clang -I@CMAKE_SOURCE_DIR@/include -I@GPUVERIFY_BENCHMARKING_DIR@/include_cl -emit-llvm -Xclang -fcuda-is-device -c -o $BASE.bc "$@"
-@LLVM_BINDIR@/opt -mem2reg -o $BASE.opt.bc $BASE.bc
+@LLVM_BINDIR@/opt -mem2reg -globaldce -o $BASE.opt.bc $BASE.bc
 @CMAKE_BINARY_DIR@/bugle -l cu -o $BASE.bpl $BASE.opt.bc
