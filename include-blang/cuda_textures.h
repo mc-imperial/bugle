47ed129d0a1ab987b621843bf3dc80a67058c1a1
Jeroen Ketema <j.ketema@imperial.ac.uk>
2013-08-30 11:06:40 +0100

    CUDA textures rewrite part 2
    Patch by Ethel Bardsley

--- include-blang/cuda_textures.h
+++ include-blang/cuda_textures.h
@@ -391,4 +391,111 @@ MK_GETFLOAT(char)
 MK_GETFLOAT(short)
 #undef MK_GETFLOAT
 
+/* texture_indirect_functions.h */
+
+template <class T>
+__device__ void tex1Dfetch(T *retVal, cudaTextureObject_t texObject, int x);
+template <class T>
+__device__ T tex1Dfetch(cudaTextureObject_t texObject, int x);
+
+template <class T>
+__device__ void tex1D(T *retVal, cudaTextureObject_t texObject, float x);
+template <class T>
+__device__ T tex1D(cudaTextureObject_t texObject, float x);
+
+template <class T>
+__device__ void tex2D(T *retVal, cudaTextureObject_t texObject, float x, float y);
+template <class T>
+__device__ T tex2D(cudaTextureObject_t texObject, float x, float y);
+
+template <class T>
+__device__ void tex3D(T *retVal, cudaTextureObject_t texObject, float x, float y, float z);
+template <class T>
+__device__ T tex3D(cudaTextureObject_t texObject, float x, float y, float z);
+
+template <class T>
+__device__ void tex1DLayered(T *retVal, cudaTextureObject_t texObject, float x, int layer);
+template <class T>
+__device__ T tex1DLayered(cudaTextureObject_t texObject, float x, int layer);
+
+template <class T>
+__device__ void tex2DLayered(T *retVal, cudaTextureObject_t texObject, float x, float y, int layer);
+template <class T>
+__device__ T tex2DLayered(cudaTextureObject_t texObject, float x, float y, int layer);
+
+template <class T>
+__device__ void texCubemap(T *retVal, cudaTextureObject_t texObject, float x, float y, float z);
+template <class T>
+__device__ T texCubemap(cudaTextureObject_t texObject, float x, float y, float z);
+
+template <class T>
+__device__ void texCubemapLayered(T *retVal, cudaTextureObject_t texObject, float x, float y, float z, int layer);
+template <class T>
+__device__ T texCubemapLayered(cudaTextureObject_t texObject, float x, float y, float z, int layer);
+
+template <class T>
+__device__ void tex2Dgather(T *retVal, cudaTextureObject_t texObject, float x, float y, int comp = 0);
+template <class T>
+__device__ T tex2Dgather(cudaTextureObject_t to, float x, float y, int comp = 0);
+
+template <class T>
+__device__ void tex1DLod(T *retVal, cudaTextureObject_t texObject, float x, float level);
+template <class T>
+__device__ T tex1DLod(cudaTextureObject_t texObject, float x, float level);
+
+template <class T>
+__device__ void tex2DLod(T *retVal, cudaTextureObject_t texObject, float x, float y, float level);
+template <class T>
+__device__ T tex2DLod(cudaTextureObject_t texObject, float x, float y, float level);
+
+template <class T>
+__device__ void tex3DLod(T *retVal, cudaTextureObject_t texObject, float x, float y, float z, float level);
+template <class T>
+__device__ T tex3DLod(cudaTextureObject_t texObject, float x, float y, float z, float level);
+
+template <class T>
+__device__ void tex1DLayeredLod(T *retVal, cudaTextureObject_t texObject, float x, int layer, float level);
+template <class T>
+__device__ T tex1DLayeredLod(cudaTextureObject_t texObject, float x, int layer, float level);
+
+template <class T>
+__device__ void tex2DLayeredLod(T *retVal, cudaTextureObject_t texObject, float x, float y, int layer, float level);
+template <class T>
+__device__ T tex2DLayeredLod(cudaTextureObject_t texObject, float x, float y, int layer, float level);
+
+template <class T>
+__device__ void texCubemapLod(T *retVal, cudaTextureObject_t texObject, float x, float y, float z, float level);
+template <class T>
+__device__ T texCubemapLod(cudaTextureObject_t texObject, float x, float y, float z, float level);
+
+template <class T>
+__device__ void texCubemapLayeredLod(T *retVal, cudaTextureObject_t texObject, float x, float y, float z, int layer, float level);
+template <class T>
+__device__ T texCubemapLayeredLod(cudaTextureObject_t texObject, float x, float y, float z, int layer, float level);
+
+template <class T>
+__device__ void tex1DGrad(T *retVal, cudaTextureObject_t texObject, float x, float dPdx, float dPdy);
+template <class T>
+__device__ T tex1DGrad(cudaTextureObject_t texObject, float x, float dPdx, float dPdy);
+
+template <class T>
+__device__ void tex2DGrad(T *retVal, cudaTextureObject_t texObject, float x, float y, float2 dPdx, float2 dPdy);
+template <class T>
+__device__ T tex2DGrad(cudaTextureObject_t texObject, float x, float y, float2 dPdx, float2 dPdy);
+
+template <class T>
+__device__ void tex3DGrad(T *retVal, cudaTextureObject_t texObject, float x, float y, float z, float4 dPdx, float4 dPdy);
+template <class T>
+__device__ T tex3DGrad(cudaTextureObject_t texObject, float x, float y, float z, float4 dPdx, float4 dPdy);
+
+template <class T>
+__device__ void tex1DLayeredGrad(T *retVal, cudaTextureObject_t texObject, float x, int layer, float dPdx, float dPdy);
+template <class T>
+__device__ T tex1DLayeredGrad(cudaTextureObject_t texObject, float x, int layer, float dPdx, float dPdy);
+
+template <class T>
+__device__ void tex2DLayeredGrad(T *retVal, cudaTextureObject_t texObject, float x, float y, int layer, float2 dPdx, float2 dPdy);
+template <class T>
+__device__ T tex2DLayeredGrad(cudaTextureObject_t texObject, float x, float y, int layer, float2 dPdx, float2 dPdy);
+
 #endif
