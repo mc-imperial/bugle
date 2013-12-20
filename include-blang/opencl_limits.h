709cad6053079b3f40b210cfd009b5fa4c477560
Jeroen Ketema <j.ketema@imperial.ac.uk>
2013-12-20 10:05:32 +0100

    Remove limits that are now in libclc

--- include-blang/opencl_limits.h
+++ include-blang/opencl_limits.h
@@ -1,27 +1,6 @@
 #ifndef OPENCL_LIMITS_H
 #define OPENCL_LIMITS_H
 
-#define FLT_DIG 6
-#define FLT_MANT_DIG  24
-#define FLT_MAX_10_EXP  +38
-#define FLT_MAX_EXP +128
-#define FLT_MIN_10_EXP  -37
-#define FLT_MIN_EXP -125
-#define FLT_RADIX 2
-#define FLT_MAX 0x1.fffffep127f
-#define FLT_MIN 0x1.0p-126f
-#define FLT_EPSILON 0x1.0p-23f
-
-#define DBL_DIG 15
-#define DBL_MANT_DIG  53
-#define DBL_MAX_10_EXP  +308
-#define DBL_MAX_EXP +1024
-#define DBL_MIN_10_EXP  -307
-#define DBL_MIN_EXP -1021
-#define DBL_MAX 0x1.fffffffffffffp1023
-#define DBL_MIN 0x1.0p-1022
-#define DBL_EPSILON 0x1.0p-52
-
 #define HALF_DIG  3
 #define HALF_MANT_DIG 11
 #define HALF_MAX_10_EXP +4
