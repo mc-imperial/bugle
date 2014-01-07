0910a4c2e52f16bb1c61ba103ef7b58b06fb7853
Jeroen Ketema <j.ketema@imperial.ac.uk>
2014-01-07 02:05:35 +0000

    use unsafe assert

--- include-blang/cuda_math_functions.h
+++ include-blang/cuda_math_functions.h
@@ -27,8 +27,8 @@ __device__ static __inline__ int __mul24(int x, int y) {
      - result only well-defined for input values in range [-2^23, 2^23-1]
      - truncation to the 32 least significant bits of the result is automatic
   */
-  __assert(x >= -8388608 && x < 8388608);
-  __assert(y >= -8388608 && y < 8388608);
+  __unsafe_assert(x >= -8388608 && x < 8388608);
+  __unsafe_assert(y >= -8388608 && y < 8388608);
   return x * y;
 }
 
