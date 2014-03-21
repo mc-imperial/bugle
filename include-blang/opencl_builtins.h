176611b278298264e4b9822953c47baade3c8f38
Jeroen Ketema <j.ketema@imperial.ac.uk>
2014-03-21 16:53:31 +0000

    sincos is now part of libclc

--- include-blang/opencl_builtins.h
+++ include-blang/opencl_builtins.h
@@ -73,28 +73,6 @@ _POWN_MACRO(float16, int16)
 
 #undef _POWN_MACRO
 
-#define _SINCOS_MACRO_FULL(GENTYPE, MEMORYSPACE) \
-    _CLC_INLINE _CLC_OVERLOAD GENTYPE sincos(GENTYPE x, MEMORYSPACE GENTYPE *cosval) { \
-        *cosval = cos(x); \
-        return sin(x); \
-    }
-
-#define _SINCOS_MACRO(GENTYPE)\
-    _SINCOS_MACRO_FULL(GENTYPE, __global) \
-    _SINCOS_MACRO_FULL(GENTYPE, __local) \
-    _SINCOS_MACRO_FULL(GENTYPE, __private)
-
-_SINCOS_MACRO(float)
-_SINCOS_MACRO(float2)
-_SINCOS_MACRO(float3)
-_SINCOS_MACRO(float4)
-_SINCOS_MACRO(float8)
-_SINCOS_MACRO(float16)
-
-#undef _SINCOS_MACRO_FULL
-#undef _SINCOS_MACRO
-
-
 // Table 6.9
 
 _FLOAT_UNARY_MACRO(half_cos)
