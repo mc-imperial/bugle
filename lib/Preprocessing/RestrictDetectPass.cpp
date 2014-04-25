e02a6da72568465b9ad49d04e9bd02c7c03686b3
Jeroen Ketema <j.ketema@imperial.ac.uk>
2014-04-25 22:57:36 +0100

    File name printing consistent with other tools

--- lib/Preprocessing/RestrictDetectPass.cpp
+++ lib/Preprocessing/RestrictDetectPass.cpp
@@ -23,12 +23,9 @@ std::string RestrictDetectPass::getFunctionLocation(llvm::Function *F) {
   for (auto i = DIF.subprogram_begin(), e = DIF.subprogram_end(); i != e; ++i) {
     DISubprogram subprogram(*i);
     if (subprogram.describes(F)) {
-      SmallString<256> path;
-      sys::path::append(path, subprogram.getDirectory());
-      sys::path::append(path, subprogram.getFilename());
       std::string l; llvm::raw_string_ostream lS(l);
       lS << "'" << subprogram.getName() << "' on line "
-         << subprogram.getLineNumber() << " of " << path;
+         << subprogram.getLineNumber() << " of " << subprogram.getFilename();
       return lS.str();
     }
   }
