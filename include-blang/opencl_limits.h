216cc8d55161b63be9e1af4d2d108b1b3c289d05
Jeroen Ketema <j.ketema@imperial.ac.uk>
2014-06-24 11:06:44 +0100

    Remove constants that are now in libclc

--- include-blang/opencl_limits.h
+++ include-blang/opencl_limits.h
@@ -1,12 +1,6 @@
 #ifndef OPENCL_LIMITS_H
 #define OPENCL_LIMITS_H
 
-#define HALF_DIG  3
-#define HALF_MANT_DIG 11
-#define HALF_MAX_10_EXP +4
-#define HALF_MAX_EXP  +16
-#define HALF_MIN_10_EXP -4
-#define HALF_MIN_EXP  -13
 #define HALF_MAX  0x1.ffcp15h
 #define HALF_MIN  0x1.0p-14h
 #define HALF_EPSILON  0x1.0p-10h
