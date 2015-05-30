af27199a6948f17260ff65d671171d0cc15ce0d3
Jeroen Ketema <j.ketema@imperial.ac.uk>
2015-05-30 02:50:53 +0100

    Change binary implementation as unary one

--- include-blang/clc/math/atan2.h
+++ include-blang/clc/math/atan2.h
@@ -1,5 +1,3 @@
 #define __CLC_FUNC_NAME __bugle_atan2
-#define __CLC_BODY <clc/math/binary_def.inc>
-#include <clc/math/gentype.inc>
-#undef __CLC_FUNC_NAME
+#include <clc/math/binary_def.inc>
 #define atan2 __bugle_atan2
