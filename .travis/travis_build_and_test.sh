cd7551211d3cc18fd2159c03aac8c5de63203879
Jeroen Ketema <j.ketema@xs4all.nl>
2017-12-24 19:37:18 +0100

    Synchronise with changes made in GPUVerify's Travis scripts

--- .travis/travis_build_and_test.sh
+++ .travis/travis_build_and_test.sh
@@ -4,6 +4,8 @@ set -ev
 export GPUVERIFY_DIR=${BUILD_ROOT}/gpuverify
 export BUGLE_DIR=${BUILD_ROOT}
 export DOWNLOADS_DIR=${BUILD_ROOT}/downloads
+
 git clone --depth=${CLONE_DEPTH} \
   --branch=master https://github.com/mc-imperial/gpuverify.git
+
 ${GPUVERIFY_DIR}/.travis/build_and_test.sh
