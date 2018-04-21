85099805bc1ffb136c0568535d6db565a36f5e4a
Jeroen Ketema <j.ketema@xs4all.nl>
2018-04-21 23:51:58 +0200

    Override libclc's fmod and hypot

---
+++ include-blang/clc/math/fmod.h
@@ -0,0 +1,5 @@
+#define fmod __bugle_fmod
+
+#define __CLC_FUNCTION __bugle_fmod
+#define __CLC_INTRINSIC "frem"
+#include <clc/math/binary_intrin.inc>
---
+++ include-blang/clc/math/hypot.h
@@ -0,0 +1,3 @@
+#define __CLC_FUNC_NAME __bugle_hypot
+#include <clc/math/binary_def.inc>
+#define hypot __bugle_hypot
