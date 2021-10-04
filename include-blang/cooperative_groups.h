7f8a426e73d4604618a7cb87d0e14fe7c70ec19d
Gautam Muduganti <cs17resch01003@iith.ac.in>
2021-10-04 18:30:28 +0530

    Changing tabs to spaces.
    Changing tabs to spaces.

--- include-blang/cooperative_groups.h
+++ include-blang/cooperative_groups.h
@@ -5,14 +5,14 @@ namespace cooperative_groups {
   class grid_group {
     public:
       __forceinline__ __device__ void sync() const {
-		  bugle_grid_barrier();
+          bugle_grid_barrier();
       }
   };
   
   class thread_block {
     public:
       __forceinline__ __device__ void sync() const {
-		  bugle_barrier(true, true);
+          bugle_barrier(true, true);
       }
   };
   
@@ -21,11 +21,11 @@ namespace cooperative_groups {
   __forceinline__ __device__ thread_block this_thread_block();
   
   __forceinline__ __device__ void synchronize(grid_group g) {
-	  bugle_grid_barrier();
+      bugle_grid_barrier();
   }
   
   __forceinline__ __device__ void synchronize(thread_block t) {
-	  bugle_barrier(true, true);
+      bugle_barrier(true, true);
   }
 }
 
