2d111e8f7d6ac70e74f45b841959eb315d6e8548
Gautam Muduganti <cs17resch01003@iith.ac.in>
2022-03-13 17:32:40 +0530

    Renaming the header file
    Renaming the header file.

---
+++ include-blang/cuda_cooperative_groups.h
@@ -0,0 +1,32 @@
+#ifndef CUDA_COOPERATIVE_GROUPS_H
+#define CUDA_COOPERATIVE_GROUPS_H
+
+namespace cooperative_groups {
+  class grid_group {
+    public:
+      __inline__ __device__ void sync() const {
+          bugle_grid_barrier();
+      }
+  };
+  
+  class thread_block {
+    public:
+      __inline__ __device__ void sync() const {
+          bugle_barrier(true, true);
+      }
+  };
+  
+  __device__ grid_group this_grid();
+  
+  __device__ thread_block this_thread_block();
+  
+  __inline__ __device__ void synchronize(grid_group g) {
+      bugle_grid_barrier();
+  }
+  
+  __inline__ __device__ void synchronize(thread_block t) {
+      bugle_barrier(true, true);
+  }
+}
+
+#endif
